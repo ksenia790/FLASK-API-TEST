d0b65060762d5d64acc8fc8237e68f101af68a71
KseniyaSaltik <irbis.mr@gmail.com>
2022-05-04 17:31:05 +0300

    Addin __init__.py,config and models modules

---
+++ models.py
@@ -0,0 +1,19 @@
+import flask_sqlalchemy
+
+db = flask_sqlalchemy.SQLAlchemy()
+
+# Configure table
+class Quizz(db.Model):
+    
+    id = db.Column(db.Integer(),primary_key=True)
+    question_id = db.Column(db.Integer(), unique=True)
+    answer = db.Column(db.String(100),nullable=False)
+    question = db.Column(db.Text(),nullable=False)
+    date = db.Column(db.String(100),nullable=False)
+
+    def __repr__(self):
+        return self.name
+
+    def save(self):
+        db.session.add(self)
+        db.session.commit()
