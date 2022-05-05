f9372b15802cb0ab7df93dc0fbb37af5909cd461
KseniyaSaltik <irbis.mr@gmail.com>
2022-05-05 20:28:43 +0300

    Additional comments

--- __init__.py
+++ __init__.py
@@ -4,7 +4,8 @@ from .models import db
 
 def create_app():
     flask_app = Flask(__name__)
-    flask_app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql+psycopg2://postgres:sena@localhost/' 
+    # change <name> and <host> below according to your own
+    flask_app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql+psycopg2://postgres:<name>@<host>/' 
     flask_app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
     flask_app.app_context().push()
     db.init_app(flask_app)
